15eb6560ee5f9c76dc1600d230b2eefe49c364f4
gonpeche <espeche.g@gmail.com>
2018-12-31 18:51:35 -0300

    update style

--- src/Containers/TodoContainer.js
+++ src/Containers/TodoContainer.js
@@ -1,89 +1,39 @@
-// import React, { Component } from 'react'
-
-// export default class TodoContainer extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <ul>
-//         {
-//             this.props.list.length > 0 ?
-//                 this.props.list.map( function(todo, i) { 
-//                     return <li key={i}>{todo}</li>
-//                 }) : null
-//         }
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { Component } from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
+import { withStyles } from '@material-ui/core/styles'
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-});
 
-class CheckboxList extends React.Component {
-  state = {
-    checked: [0],
-  };
+const styles = () => ({
+    margin: {
+        margin: "0px",
+        padding: "0px",
+        float: "right"
+    },
+  });
 
-  handleToggle = value => () => {
-    const { checked } = this.state;
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    this.setState({
-      checked: newChecked,
-    });
-  };
-
+class TodoContainer extends Component {
   render() {
-    const { classes } = this.props;
-
+      const { classes } = this.props
     return (
-      <List className={classes.root}>
-        {[0, 1, 2, 3].map(value => (
-          <ListItem key={value} role={undefined} dense button onClick={this.handleToggle(value)}>
-            <Checkbox
-              checked={this.state.checked.indexOf(value) !== -1}
-              tabIndex={-1}
-              disableRipple
-            />
-            <ListItemText primary={`Line item ${value + 1}`} />
-            <ListItemSecondaryAction>
-              <IconButton aria-label="Comments">
-                <CommentIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        ))}
-      </List>
-    );
+        <React.Fragment>
+        <ul className="ul">
+        {
+            this.props.list.length > 0 ?
+                this.props.list.map( function(todo, i) { 
+                    return (
+                        <li key={i}> {todo}
+                            <IconButton aria-label="Delete" className={classes.margin}>
+                                <DeleteIcon fontSize="small" />
+                            </IconButton>
+                        </li>
+                    )
+                }) : null
+        }
+        </ul>        
+        </React.Fragment>
+    )
   }
 }
 
-CheckboxList.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(CheckboxList);
+export default withStyles(styles)(TodoContainer)
